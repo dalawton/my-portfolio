f33ba79db0ada3f2397152329e7e89e5028c9a1e
Danielle Lawton <Daniellelawton8@gmail.com>
2025-07-15 12:26:59 -0400

    added link and resized image

--- pages/index.js
+++ pages/index.js
@@ -176,14 +176,10 @@ export default function Home() {
                         ))}
                       </div>
                     </div>
-                    <div className={`${projectsStyles.expandIcon} ${expandedCard === proj.id ? projectsStyles.expanded : ''}`}>
-                      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                        <polyline points="6,9 12,15 18,9"></polyline>
-                      </svg>
-                    </div>
                   </div>
                   <div className={projectsStyles.cardDescription}>
                     <p>{proj.description}</p>
+                    <p>{proj.link}</p>
                   </div>
                 </div>
               </div>
